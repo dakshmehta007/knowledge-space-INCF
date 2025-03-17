195eeae92a487cde75a7eeb4086669572b1978b2
dakshmehta007 <mehtadaksh85@gmail.com>
2025-03-18 01:11:11 +0530

    Initial setup for ScrollToTop Button

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ScrollToTop from "./components/ScrollToTop";
+
+function App() {
+  return (
+    <div className="app">
+      <ScrollToTop />
+    </div>
+  );
+}
+
+export default App;
